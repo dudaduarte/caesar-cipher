19fa0d8b153f8f0157c77ea40b703b84208c0682
Duda Duarte <dudammduarte@yahoo.com.br>
2019-01-24 17:08:48 -0200

    coloquei as funções de cifrar e decifrar, e criei na de cifrar uma array com os codigos ASC dos caracteres do texto inserido

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,28 @@
 function submitButton (){
-  const textInformation = document.getElementById('idTextUser').value;
-  let arrayText = textInformation.split('');
-  document.getElementById('showText').innerHTML = arrayText;
-  return arrayText;
+  const string = document.getElementById('idTextUser').value;
+  //let arrayText = string.split('');
+  //document.getElementById('showText').innerHTML = arrayText;
+  return string;
 }
+
+cipher.encode(offset, string){
+  let i = 0;
+  //lastIndex = string.length-1;
+  arrayAsc = [];
+
+  while (i < string.legth) {
+  arrayAsc.push(string.charCodeAt(i));
+  i++;
+}
+
+
+}
+
+/* cipher.decode(offset, string){
+
+} */
+
+
 
 let textInformation = submitButton();
 
