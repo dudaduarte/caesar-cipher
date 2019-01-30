98e9fefbb2b54454195bd1f49903f920c5518000
Duda Duarte <dudammduarte@yahoo.com.br>
2019-01-29 22:54:42 -0200

    a parte funcional tá quase ok, falta verificar umas dúvidas q tão no trello. E CSS TB

--- src/cipher.js
+++ src/cipher.js
@@ -8,45 +8,55 @@ function showDivDecode() {
   document.getElementById('decode').classList.remove('hidden');
 }
 
+function clearText() {
+  document.getElementById('idFormEncode').reset();
+  document.getElementById('idFormDecode').reset();
+}
+
+function copyText() {
+  document.getElementById('idTextModified').focus();
+  document.execCommand('selectAll');
+  document.execCommand('copy');
+}
+
+function pasteText() {
+  document.getElementById('idTextUser2').select();
+  document.execCommand('paste');
+}
+
+// /\ não tá funcionando
+
 function startAgain() {
   document.getElementById('welcome').classList.remove('hidden');
   document.getElementById('code').classList.add('hidden');
   document.getElementById('decode').classList.add('hidden');
-  // /\ verificar se isso de value="" é realmente necessário
-  // verificar se tem uma maneira menos feia de copiar o conteúdo de uma função pra outra
+  clearText();
 }
 
-function clearText() {
-  document.getElementById('idTextUser').value="";
-  document.getElementById('idTextUser2').value="";
-  document.getElementById('idTextModified').value="";
-  document.getElementById('idTextModified2').value="";
-  document.getElementById('offsetInput').value="0";
-  document.getElementById('offsetInput2').value="0";
-  document.getElementById('showNumber').value="0";
-  document.getElementById('showNumber2').value="0";  
-}
-
-/*function getInformations() {
+function getInformationsEncode() {
   string = document.getElementById('idTextUser').value;
   offset = parseInt(document.getElementById('offsetInput').value);
 
-  document.getElementById('idTextModified').innerHTML = cipherEncode(offset,string);
-}*/
+  document.getElementById('idTextModified').innerHTML = cipherEncode(offset, string);
+}
 
-function cipherEncode(offset, string){
-  string = document.getElementById('idTextUser').value;
-  // /\ essa parte que pega informação do html também precisa ser retirada daqui
+function getInformationsDecode() {
+  string = document.getElementById('idTextUser2').value;
+  offset = parseInt(document.getElementById('offsetInput2').value);
+
+  document.getElementById('idTextModified2').innerHTML = cipherDecode(offset, string);
+}
+
+function cipherEncode(offset, string) {
   arrayAsc = [];
 
-  for (i = 0; i < string.length; i++){
+  for (i = 0; i < string.length; i++) {
     arrayAsc.push(string.charCodeAt(i));
   }
 
-  offset = parseInt(document.getElementById('offsetInput').value);
   arrayEncripted = [];
 
-  for (j = 0; j < arrayAsc.length; j++){
+  for (j = 0; j < arrayAsc.length; j++) {
     if (arrayAsc[j] >= 97 && arrayAsc[j] <= 122) {
       arrayEncripted.push(([arrayAsc[j] - 97 + offset] % 26) + 97);
     } else if (arrayAsc[j] >= 65 && arrayAsc[j] <= 90) {
@@ -57,26 +67,19 @@ function cipherEncode(offset, string){
   }
 
   let textEncripted = String.fromCharCode(...arrayEncripted);
-
-  document.getElementById('idTextModified').innerHTML=textEncripted;
-// /\ deixar tudo que for "externo", que diga respeito ao HTM etc, FORA dessa função, em outra função.
-return textEncripted;
+  return textEncripted;
 }
 
-
-
-function cipherDecode(offset, string){
-  string = document.getElementById('idTextUser2').value;
+function cipherDecode(offset, string) {
   arrayAsc = [];
 
-  for (i = 0; i < string.length; i++){
+  for (i = 0; i < string.length; i++) {
     arrayAsc.push(string.charCodeAt(i));
   }
 
-  offset = parseInt(document.getElementById('offsetInput2').value);
   arrayDisencripted = [];
 
-  for (j = 0; j < arrayAsc.length; j++){
+  for (j = 0; j < arrayAsc.length; j++) {
     if (arrayAsc[j] >= 97 && arrayAsc[j] <= 122) {
       arrayDisencripted.push(([arrayAsc[j] - 122 - offset] % 26) + 122);
     } else if (arrayAsc[j] >= 65 && arrayAsc[j] <= 90) {
@@ -87,15 +90,12 @@ function cipherDecode(offset, string){
   }
 
   let textDisencripted = String.fromCharCode(...arrayDisencripted);
-
-  document.getElementById('idTextModified2').innerHTML=textDisencripted;
-
-return textDisencripted;
+  return textDisencripted;
 }
 
 
 /*
 window.cipher = {
-  // ... 
+  // ...
 };
 */
